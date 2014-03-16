58582fb54e680d9bc2a07b5e6e6b9433b0149b28
ayoola <ayoola_al@yahoo.com>
2014-03-16 22:59:38 +0000

    deleted

--- local/Booking_App/Booking_App_Project/navigation.php
+++
@@ -1,5 +0,0 @@
- <?php
- // navigation bar 
- echo" <div class=navigation><ul><tab> | </tab><a href =index.php>Home Page</a><tab> | </tab><a href = validate_reg_input.php> Register</a><tab> | </tab> <a href = log_on.php>Check Your Booking</a>
- <tab> | </tab> <a href =index.php#chooseseat>Book Seats</a><tab> | </tab></ul></div>";
- ?>
--- local/Booking_App/Booking_App_Project/sql_scripts/Production_insert.sql
+++
@@ -1,3 +0,0 @@
-insert into  Production values( 'Macbeth' , 1.00);
-insert into  Production values( 'Othello' , 1.50);
-insert into Production values ('Romeo and Juliet', 2.00);
